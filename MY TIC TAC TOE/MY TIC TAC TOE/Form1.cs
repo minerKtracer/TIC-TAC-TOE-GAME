835dd238c5b591a06fb986532399106f0df3bf3a
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-06-05 18:46:24 +0800

    Checking for winner

--- MY TIC TAC TOE/MY TIC TAC TOE/Form1.cs
+++ MY TIC TAC TOE/MY TIC TAC TOE/Form1.cs
@@ -31,6 +31,27 @@ namespace MY_TIC_TAC_TOE
                 {
                     turn = 'X';
                 }
+                if (FB1.Text == SB1.Text)
+                {
+                    if (SB1.Text == TB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB1.Text);
+                    }
+               }
+                if (FB1.Text == FB2.Text)
+                {
+                    if (FB2.Text == FB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB1.Text);
+                    }
+                }
+                if (FB1.Text == SB2.Text)
+                {
+                    if (SB2.Text == TB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB1.Text);
+                    }
+                }
             }
         }
 
@@ -46,6 +67,20 @@ namespace MY_TIC_TAC_TOE
                 else
                 {
                     turn = 'X';
+                }
+                if (FB2.Text == SB2.Text)
+                {
+                    if (SB2.Text == TB2.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB2.Text);
+                    }
+                }
+                if (FB2.Text == FB1.Text)
+                {
+                    if (FB1.Text == FB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB2.Text);
+                    }
                 }
             }
         }
@@ -63,6 +98,27 @@ namespace MY_TIC_TAC_TOE
                 {
                     turn = 'X';
                 }
+                if (FB3.Text == SB3.Text)
+                {
+                    if (SB3.Text == TB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB3.Text);
+                    }
+                }
+                if (FB3.Text == FB2.Text)
+                {
+                    if (FB2.Text == FB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB3.Text);
+                    }
+                }
+                if (FB3.Text == SB2.Text)
+                {
+                    if (SB2.Text == TB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + FB3.Text);
+                    }
+                }
             }
         }
 
@@ -78,6 +134,20 @@ namespace MY_TIC_TAC_TOE
                 else
                 {
                     turn = 'X';
+                }
+                if (SB1.Text == SB2.Text)
+                {
+                    if (SB2.Text == SB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB1.Text);
+                    }
+                }
+                if (SB1.Text == FB1.Text)
+                {
+                    if (FB1.Text == TB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB1.Text);
+                    }
                 }
             }
         }
@@ -95,6 +165,20 @@ namespace MY_TIC_TAC_TOE
                 {
                     turn = 'X';
                 }
+                if (SB2.Text == SB1.Text)
+                {
+                    if (SB1.Text == SB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB2.Text);
+                    }
+                }
+                if (SB2.Text == FB2.Text)
+                {
+                    if (FB2.Text == TB2.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB2.Text);
+                    }
+                }
             }
         }
 
@@ -110,6 +194,20 @@ namespace MY_TIC_TAC_TOE
                 else
                 {
                     turn = 'X';
+                }
+                if (SB3.Text == SB2.Text)
+                {
+                    if (SB2.Text == SB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB3.Text);
+                    }
+                }
+                if (SB3.Text == FB3.Text)
+                {
+                    if (FB3.Text == TB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + SB3.Text);
+                    }
                 }
             }
         }
@@ -127,6 +225,27 @@ namespace MY_TIC_TAC_TOE
                 {
                     turn = 'X';
                 }
+                if (TB1.Text == SB1.Text)
+                {
+                    if (SB1.Text == FB1.Text)
+                    {
+                        MessageBox.Show("The winner is " + TB1.Text);
+                    }
+                }
+                if (TB1.Text == SB2.Text)
+                {
+                    if (SB2.Text == FB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + TB1.Text);
+                    }
+                }
+                if (TB1.Text == TB2.Text)
+                {
+                    if (TB2.Text == TB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + TB1.Text);
+                    }
+                }
             }
         }
 
@@ -143,6 +262,20 @@ namespace MY_TIC_TAC_TOE
                 {
                     turn = 'X';
                 }
+                if (TB2.Text == SB2.Text)
+                {
+                    if (SB2.Text == FB2.Text)
+                    {
+                        MessageBox.Show("The winner is " + TB2.Text);
+                    }
+                }
+                if (TB2.Text == TB1.Text)
+                {
+                    if (TB1.Text == TB3.Text)
+                    {
+                        MessageBox.Show("The winner is " + TB2.Text);
+                    }
+                }
             }
         }
 
@@ -158,6 +291,27 @@ namespace MY_TIC_TAC_TOE
                 else
                 {
                     turn = 'X';
+                }
+            }
+            if (TB3.Text == SB3.Text)
+            {
+                if (SB3.Text == FB3.Text)
+                {
+                    MessageBox.Show("The winner is " + TB3.Text);
+                }
+            }
+            if (TB3.Text == SB2.Text)
+            {
+                if (SB2.Text == FB1.Text)
+                {
+                    MessageBox.Show("The winner is " + TB3.Text);
+                }
+            }
+            if (TB3.Text == TB2.Text)
+            {
+                if (TB2.Text == TB1.Text)
+                {
+                    MessageBox.Show("The winner is " + TB3.Text);
                 }
             }
         }
