2eda81280362fb0a752855384136663a3f85157f
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-06-05 20:35:36 +0800

    Buttons and form color scheme

--- MY TIC TAC TOE/MY TIC TAC TOE/Form1.Designer.cs
+++ MY TIC TAC TOE/MY TIC TAC TOE/Form1.Designer.cs
@@ -47,96 +47,108 @@
             // 
             // FB1
             // 
+            this.FB1.BackColor = System.Drawing.Color.LightCyan;
             this.FB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FB1.Location = new System.Drawing.Point(24, 47);
             this.FB1.Name = "FB1";
             this.FB1.Size = new System.Drawing.Size(74, 70);
             this.FB1.TabIndex = 0;
-            this.FB1.UseVisualStyleBackColor = true;
+            this.FB1.UseVisualStyleBackColor = false;
             this.FB1.Click += new System.EventHandler(this.FB1_Click);
             // 
             // FB2
             // 
+            this.FB2.BackColor = System.Drawing.Color.LightCyan;
             this.FB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FB2.Location = new System.Drawing.Point(104, 47);
             this.FB2.Name = "FB2";
             this.FB2.Size = new System.Drawing.Size(74, 70);
             this.FB2.TabIndex = 1;
-            this.FB2.UseVisualStyleBackColor = true;
+            this.FB2.UseVisualStyleBackColor = false;
             this.FB2.Click += new System.EventHandler(this.FB2_Click);
             // 
             // FB3
             // 
+            this.FB3.BackColor = System.Drawing.Color.LightCyan;
             this.FB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FB3.Location = new System.Drawing.Point(184, 47);
             this.FB3.Name = "FB3";
             this.FB3.Size = new System.Drawing.Size(74, 70);
             this.FB3.TabIndex = 2;
-            this.FB3.UseVisualStyleBackColor = true;
+            this.FB3.UseVisualStyleBackColor = false;
             this.FB3.Click += new System.EventHandler(this.FB3_Click);
             // 
             // SB1
             // 
+            this.SB1.BackColor = System.Drawing.Color.LightCyan;
             this.SB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SB1.Location = new System.Drawing.Point(24, 123);
             this.SB1.Name = "SB1";
             this.SB1.Size = new System.Drawing.Size(74, 70);
             this.SB1.TabIndex = 3;
-            this.SB1.UseVisualStyleBackColor = true;
+            this.SB1.UseVisualStyleBackColor = false;
             this.SB1.Click += new System.EventHandler(this.SB1_Click);
             // 
             // SB2
             // 
+            this.SB2.BackColor = System.Drawing.Color.LightCyan;
             this.SB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SB2.Location = new System.Drawing.Point(104, 123);
             this.SB2.Name = "SB2";
             this.SB2.Size = new System.Drawing.Size(74, 70);
             this.SB2.TabIndex = 4;
-            this.SB2.UseVisualStyleBackColor = true;
+            this.SB2.UseVisualStyleBackColor = false;
             this.SB2.Click += new System.EventHandler(this.SB2_Click);
             // 
             // SB3
             // 
+            this.SB3.BackColor = System.Drawing.Color.LightCyan;
             this.SB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SB3.Location = new System.Drawing.Point(184, 123);
             this.SB3.Name = "SB3";
             this.SB3.Size = new System.Drawing.Size(74, 70);
             this.SB3.TabIndex = 5;
-            this.SB3.UseVisualStyleBackColor = true;
+            this.SB3.UseVisualStyleBackColor = false;
             this.SB3.Click += new System.EventHandler(this.SB3_Click);
             // 
             // TB1
             // 
+            this.TB1.BackColor = System.Drawing.Color.LightCyan;
             this.TB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TB1.Location = new System.Drawing.Point(24, 199);
             this.TB1.Name = "TB1";
             this.TB1.Size = new System.Drawing.Size(74, 70);
             this.TB1.TabIndex = 6;
-            this.TB1.UseVisualStyleBackColor = true;
+            this.TB1.UseVisualStyleBackColor = false;
             this.TB1.Click += new System.EventHandler(this.TB1_Click);
             // 
             // TB2
             // 
+            this.TB2.BackColor = System.Drawing.Color.LightCyan;
             this.TB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TB2.Location = new System.Drawing.Point(104, 199);
             this.TB2.Name = "TB2";
             this.TB2.Size = new System.Drawing.Size(74, 70);
             this.TB2.TabIndex = 7;
-            this.TB2.UseVisualStyleBackColor = true;
+            this.TB2.UseVisualStyleBackColor = false;
             this.TB2.Click += new System.EventHandler(this.TB2_Click);
             // 
             // TB3
             // 
+            this.TB3.BackColor = System.Drawing.Color.LightCyan;
             this.TB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TB3.Location = new System.Drawing.Point(184, 199);
             this.TB3.Name = "TB3";
             this.TB3.Size = new System.Drawing.Size(74, 70);
             this.TB3.TabIndex = 8;
-            this.TB3.UseVisualStyleBackColor = true;
+            this.TB3.UseVisualStyleBackColor = false;
             this.TB3.Click += new System.EventHandler(this.TB3_Click);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.PeachPuff;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.File1,
             this.AMenu1});
@@ -186,6 +198,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
             this.ClientSize = new System.Drawing.Size(284, 293);
             this.Controls.Add(this.TB3);
             this.Controls.Add(this.TB2);
@@ -197,6 +210,7 @@
             this.Controls.Add(this.FB2);
             this.Controls.Add(this.FB1);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Coral;
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "MainFORM1";
@@ -220,11 +234,11 @@
         private System.Windows.Forms.Button TB1;
         private System.Windows.Forms.Button TB2;
         private System.Windows.Forms.Button TB3;
-        private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem File1;
         private System.Windows.Forms.ToolStripMenuItem Extmenu1;
         private System.Windows.Forms.ToolStripMenuItem AMenu1;
         private System.Windows.Forms.ToolStripMenuItem GInfo1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
