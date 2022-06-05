317c21e4107f07628079478ac79864bf3d91c00b
minerKtracer <104286945+minerKtracer@users.noreply.github.com>
2022-06-05 13:39:47 +0800

    First Commit

---
+++ MY TIC TAC TOE/MY TIC TAC TOE/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace MY_TIC_TAC_TOE
+{
+    partial class MainFORM1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainFORM1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 282);
+            this.MaximizeBox = false;
+            this.Name = "MainFORM1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MY TIC TAC TOE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ MY TIC TAC TOE/MY TIC TAC TOE/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MY_TIC_TAC_TOE
+{
+    public partial class MainFORM1 : Form
+    {
+        public MainFORM1()
+        {
+            InitializeComponent();
+        }
+    }
+}
